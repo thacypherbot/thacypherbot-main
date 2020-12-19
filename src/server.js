7459902ce0ddb3bb36279e56039d7b2303a1dadb
Riday <ridayshah2001@gmail.com>
2020-12-19 11:47:50 +0530

    Chore: added hello world return to test server

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const app = express();
 
 const port = process.env.PORT ?? 3333;
 app.use('/public', express.static('public'));
+app.get('/', (_, res) => res.end('Hello World!'));
 app.listen(port, () => console.log(`Listening on port ${port}.`));
 
 module.exports = app;
